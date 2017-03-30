da3063833f471c1266ebb838d728862844961e78
rov <valadares.thel@gmail.com>
2017-03-30 14:56:11 -0300

    Adds alert with multiple choices

--- demo_ios/AlertHelper.swift
+++ demo_ios/AlertHelper.swift
@@ -1,11 +1,16 @@
 import UIKit
 
 struct AlertHelper {
-  static func message(viewController: UIViewController, title: String, message: String, completion: (() -> Void)? = nil) {
+  static func message(viewController: UIViewController, title: String, message: String, actions: [UIAlertAction]? = nil) {
     let alertController = UIAlertController(title: title, message: message, preferredStyle: .alert)
-    let okAction = UIAlertAction(title: "OK", style: .default) { (action) in completion?() }
-    
-    alertController.addAction(okAction)
+    if let actions = actions {
+      for action in actions {
+          alertController.addAction(action)
+      }
+    } else {
+      let okAction = UIAlertAction(title: "OK", style: .default)
+      alertController.addAction(okAction)
+    }
     viewController.present(alertController, animated: true)
   }
 }
--- demo_ios/AlertsViewController.swift
+++ demo_ios/AlertsViewController.swift
@@ -3,7 +3,54 @@ import UIKit
 class AlertsViewController: UIViewController {
   
   
+  @IBOutlet weak var choicesLabel: UILabel!
+  
+  
   @IBAction func callAlertAction(_ sender: UIButton) {
     AlertHelper.message(viewController: self, title: "Simple Alert", message: "This is a simple alert")
   }
+  
+  
+  @IBAction func callAlertChoiceAction(_ sender: UIButton) {
+    let yesAction = UIAlertAction(title: "Yes", style: .default, handler: { (action: UIAlertAction!) in
+      let text = "You choose: Yes"
+      self.choicesLabel.text = text
+    })
+    
+    let noAction = UIAlertAction(title: "No", style: .default, handler: { (action: UIAlertAction!) in
+      let text = "You choose: No"
+      self.choicesLabel.text = text
+    })
+    
+    var actions: [UIAlertAction] = []
+    actions.append(noAction)
+    actions.append(yesAction)
+    AlertHelper.message(viewController: self, title: "Choice Alert", message: "Choose something", actions: actions)
+  }
+  
+  
+  @IBAction func alertManyChoices(_ sender: UIButton) {
+    let normalAction = UIAlertAction(title: "Normal", style: .default, handler: { (action: UIAlertAction!) in
+      let text = "You choose: Normal"
+      self.choicesLabel.text = text
+    })
+    
+    let desctutiveAction = UIAlertAction(title: "Destructive", style: .destructive, handler: { (action: UIAlertAction!) in
+      let text = "You choose: Destrutive"
+      self.choicesLabel.text = text
+    })
+    
+    let cancelAction = UIAlertAction(title: "Cancel", style: .cancel, handler: { (action: UIAlertAction!) in
+      let text = "Cancel"
+      self.choicesLabel.text = text
+    })
+    
+    var actions: [UIAlertAction] = []
+    actions.append(normalAction)
+    actions.append(desctutiveAction)
+    actions.append(cancelAction)
+    AlertHelper.message(viewController: self, title: "Choice Alert", message: "Choose something", actions: actions)
+  }
+  
+  
 }
